79381fe0c3c2a4487351530fb0acbbaae3bb605b
Crowdbotics <team@crowdbotics.com>
2023-11-01 14:23:23 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tallysheet-3-44266",
+  url: "https://tallysheet-3-44266.botics.co",
+  api: "https://tallysheet-3-44266.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
